1a89c88f431e5f439b1a463213ff7366281e3373
marween <annemagali.saintgeorges@gmail.com>
2019-03-25 11:39:18 +0100

    modif page accueil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -130,19 +130,7 @@
 
     </div>
 
-        @if (Route::has('login'))
-    <div class="top-right links">
-        @auth
-        <a href="{{ url('/home') }}">Home</a>
-        @else
-        <a href="{{ route('login') }}">Login</a>
 
-        @if (Route::has('register'))
-        <a href="{{ route('register') }}">Register</a>
-        @endif
-        @endauth
-    </div>
-    @endif
 
     <div class="container">
         @yield('content')
